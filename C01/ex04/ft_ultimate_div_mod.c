50f83df0035f8eca2db4c20f4935d99eb0951f0f
Hayalet <66572885+Hayalet-adam@users.noreply.github.com>
2022-02-17 12:05:08 +0300

    Add files via upload

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcal <mcal@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 22:58:19 by mcal              #+#    #+#             */
+/*   Updated: 2022/02/17 04:31:34 by mcal             ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+#include <unistd.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+/*
+int	main(void)
+{
+	int	x;
+	int	y;
+
+	x = 15;
+	y = 3;
+	ft_ultimate_div_mod(&x, &y);
+	printf("%d | %d", x, y);
+}
+*/
