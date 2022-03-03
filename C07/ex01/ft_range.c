1a1406c6b04045771f3a11ff8ddccc30263e1add
Hayalet <66572885+Hayalet-adam@users.noreply.github.com>
2022-03-03 08:59:55 +0300

    Add files via upload

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcal <mcal@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 19:49:23 by mcal              #+#    #+#             */
+/*   Updated: 2022/03/02 19:49:30 by mcal             ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	i;
+	int	*ptr;
+
+	i = 0;
+	if (min >= max)
+		return (NULL);
+	ptr = (int *)malloc(sizeof(int) * (max - min));
+	if (ptr == NULL)
+		return (NULL);
+	while (min < max)
+	{
+		ptr[i] = min;
+		min++;
+		i++;
+	}
+	return (ptr);
+}
